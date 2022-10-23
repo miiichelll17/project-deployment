9c8c0b542123c7dfa68d4da81fdcebc82e9f8756
Michel <michel.2018@students.unila.ac.id>
2022-10-23 13:43:10 +0700

    commit 1

--- app.py
+++ app.py
@@ -1,80 +1,159 @@
-'''
-	Contoh Deloyment untuk Domain Data Science (DS)
-	Orbit Future Academy - AI Mastery - KM Batch 3
-	Tim Deployment
-	2022
-'''
-
-# =[Modules dan Packages]========================
-
-from flask import Flask,render_template,request,jsonify
-import pandas as pd
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime, time
+import os, sys
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-from joblib import load
+from threading import Thread
 
-# =[Variabel Global]=============================
 
-app   = Flask(__name__, static_url_path='/static')
-model = None
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
 
-# =[Routing]=====================================
+#make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
 
-# [Routing untuk Halaman Utama atau Home]	
-@app.route("/")
-def beranda():
+#Load pretrained face detection model    
+net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
+
+#instatiate flask app  
+app = Flask(__name__, template_folder='./templates')
+
+
+camera = cv2.VideoCapture(0)
+
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+
+def detect_face(frame):
+    global net
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))   
+    net.setInput(blob)
+    detections = net.forward()
+    confidence = detections[0, 0, 0, 2]
+
+    if confidence < 0.5:            
+            return frame           
+
+    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+    try:
+        frame=frame[startY:endY, startX:endX]
+        (h, w) = frame.shape[:2]
+        r = 480 / float(h)
+        dim = ( int(w * r), 480)
+        frame=cv2.resize(frame,dim)
+    except Exception as e:
+        pass
+    return frame
+ 
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame
+    while True:
+        success, frame = camera.read() 
+        if success:
+            if(face):                
+                frame= detect_face(frame)
+            if(grey):
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if(neg):
+                frame=cv2.bitwise_not(frame)    
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
+                cv2.imwrite(p, frame)
+            
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+    
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('grey') == 'Grey':
+            global grey
+            grey=not grey
+        elif  request.form.get('neg') == 'Negative':
+            global neg
+            neg=not neg
+        elif  request.form.get('face') == 'Face Only':
+            global face
+            face=not face 
+            if(face):
+                time.sleep(4)   
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index.html')
     return render_template('index.html')
 
-# [Routing untuk API]	
-@app.route("/api/deteksi",methods=['POST'])
-def apiDeteksi():
-	# Nilai default untuk variabel input atau features (X) ke model
-	input_sepal_length = 5.1
-	input_sepal_width  = 3.5
-	input_petal_length = 1.4
-	input_petal_width  = 0.2
-	
-	if request.method=='POST':
-		# Set nilai untuk variabel input atau features (X) berdasarkan input dari pengguna
-		input_sepal_length = float(request.form['sepal_length'])
-		input_sepal_width  = float(request.form['sepal_width'])
-		input_petal_length = float(request.form['petal_length'])
-		input_petal_width  = float(request.form['petal_width'])
-		
-		# Prediksi kelas atau spesies bunga iris berdasarkan data pengukuran yg diberikan pengguna
-		df_test = pd.DataFrame(data={
-			"SepalLengthCm" : [input_sepal_length],
-			"SepalWidthCm"  : [input_sepal_width],
-			"PetalLengthCm" : [input_petal_length],
-			"PetalWidthCm"  : [input_petal_width]
-		})
-
-		hasil_prediksi = model.predict(df_test[0:1])[0]
-
-		# Set Path untuk gambar hasil prediksi
-		if hasil_prediksi == 'Iris-setosa':
-			gambar_prediksi = '/static/images/iris_setosa.jpg'
-		elif hasil_prediksi == 'Iris-versicolor':
-			gambar_prediksi = '/static/images/iris_versicolor.jpg'
-		else:
-			gambar_prediksi = '/static/images/iris_virginica.jpg'
-		
-		# Return hasil prediksi dengan format JSON
-		return jsonify({
-			"prediksi": hasil_prediksi,
-			"gambar_prediksi" : gambar_prediksi
-		})
-
-# =[Main]========================================
 
 if __name__ == '__main__':
-	
-	# Load model yang telah ditraining
-	model = load('model_iris_dt.model')
-
-	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
-	
-	
-
-
+    app.run(port=5001)
+    
+camera.release()
+cv2.destroyAllWindows()     
